724d7ad09b82a32c2efb8e1242f34a7a4028590b
AvinashChandavar7 <122362595+AvinashChandavar7@users.noreply.github.com>
2023-07-23 17:21:43 +0530

    Update unsplashApi.js
    ApiKey in env

--- src/api/unsplashApi.js
+++ src/api/unsplashApi.js
@@ -4,10 +4,11 @@ import { toast } from 'react-toastify';
 const API_URL = 'https://api.unsplash.com/search/photos';
 const IMAGES_PER_PAGE = 20;
 
+
 const fetchImages = async (searchInputQuery, page,) => {
   try {
     const { data } = await axios.get(
-      `${API_URL}?query=${searchInputQuery}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`
+      `${API_URL}?query=${searchInputQuery}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=EzgrzKMCQHzdil9V2ZwCjXau58wIdUL1C8qkHCuwuAc`
     );
     return data;
   } catch (error) {
